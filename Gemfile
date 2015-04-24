eccc21f194e316d90f221b062c4947a6ebb0a19f
jiffybank <jiffybank@gmail.com>
2015-04-24 21:16:22 +0000

    First pass on fecthing files from S3 with a gem and reading them into an array of line objects

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
+# Use an S3 gem to access bucket and files
+# See https://github.com/qoobaa/s3
+gem 's3', '0.3.22'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,42 @@
 class ApiController < ApplicationController
+  
+  # https://github.com/qoobaa/s3
+  require "s3"
+  
+  # return array of s3 file objects
+  # [#<S3::Object:/conspire-challenge/claims_to_fame.txt>, ...]
+  def get_files_from_s3
+    # connect to s3
+    service = S3::Service.new(:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'])
+    # get bucket
+    bucket = service.buckets.find(ENV['S3_BUCKET'])
+    # return files
+    bucket.objects
+  end
+  
+  # trim an object: strip a string, ignore otherwise
+  def trim(v)
+    v.respond_to?('strip') ? v.strip : v
+  end
+  
+  # return array of line objects from array of s3 file objects
+  # [{ "f": "claims_to_fame.txt", "k": "Maroon 5", "v": "lameness" }, ... ]
+  def read_files_into_line_objects(files)
+    lines = []
+    files.each do |f|
+      filename = f.key
+      file = files.find(filename)
+      file.content.split(/\r?\n/).each do |line|
+        key, value = line.split("\t")
+        lines << { :f => filename, :k => trim(key), :v => trim(value) }
+      end
+    end
+    lines
+  end
+  
   def query
-    lines = [
-      { :f => 'gryffindor', :k => 'harry', :v => 'potter' },
-      { :f => 'gryffindor', :k => 'ron', :v => 'weasley' },
-      { :f => 'slytherin', :k => 'draco', :v => 'malfoy' }
-    ]
+    files = get_files_from_s3()
+    lines = read_files_into_line_objects(files)
     render :json => lines
   end
 end
